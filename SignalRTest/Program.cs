16cf1460bcd797b8929a10f698c82d6a26edc411
root <root@debian.CJH>
2015-03-25 00:09:02 -0700

    first commit

---
+++ SignalRTest/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Threading;
+
+namespace SignalRTest
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			var connector = new SocketConnector ();
+			connector.Listen ();
+			while (true) {
+			}
+		}
+	}
+}
---
+++ SignalRTest/SocketConnector.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using Microsoft.AspNet.SignalR.Client;
+using NLog;
+
+namespace SignalRTest
+{
+	public class SocketConnector
+	{
+		private static readonly Logger logger = LogManager.GetCurrentClassLogger();
+
+		private HubConnection _hubConnection;
+		private IHubProxy _messageProxy;
+		private IHubProxy MessageProxy
+		{
+			get 
+			{
+				if (_messageProxy != null && _hubConnection.State == ConnectionState.Connected) {
+					return _messageProxy;
+				}
+				_hubConnection = new HubConnection ("http://jeffhollansignalrtutorial.azurewebsites.net");
+				_messageProxy = _hubConnection.CreateHubProxy ("ChatHub");
+
+				_hubConnection.Start ().Wait ();
+				return _messageProxy;
+			}
+		}
+
+		private void Invoke(string method, params object[] args)
+		{
+			try
+			{
+//				Logger.Trace("Invoking method {0}", method);
+				MessageProxy.Invoke(method, args);
+			}
+			catch (Exception ex) {
+//				Logger.ErrorException (string.Format ("Invoke {0} error.", method), ex);
+			}
+		}
+
+		public void Listen()
+		{
+			MessageProxy.On<string, string> ("broadcastMessage", (name, message) => {
+				DoThis (name, message);
+			});
+		}
+
+		public void DoThis(string name, string message)
+		{
+			Console.WriteLine ("Name: {0} - Message {1}", name, message);
+		}
+
+//		public void UpdateNotificationsCount(long memberKey)
+//		{
+//			Invoke ("UpdateNotificationsCount", memberKey, 10);
+//		}
+	}
+}
+
